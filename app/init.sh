a63fad98fcffba07fcc7e3c9e24908a1122c0a06
tken2039 <tken2039@gmail.com>
2020-06-06 10:43:54 +0900

    init.shの修正

--- app/init.sh
+++ app/init.sh
@@ -1,48 +1,26 @@
-# service docker restart
-# 初期データ作成
+#!bin/bash
+
+# Create initial data.
 cd /home/isucon9-qualify/initial-data
 make
 
-# データベースのセットアップ
-cd /home/isucon9-qualify/webapp/sql
+# Setup Database.
 service mysql start
-# databaseとuserを初期化する
+cd /home/isucon9-qualify/webapp/sql
 mysql -u root < 00_create_database.sql
-# データを流し込む
+# Pour the data.
 ./init.sh
 
-# Nginxの起動
-service nginx start
-
-# アプリケーションの起動
+# Run application.
 cd /home/isucon9-qualify/webapp/go
 export GO111MODULE=on
 go build -o isucari
 ./isucari &
 
-# 初回ベンチマーク
+# The first benchmark test.
 cd /home/isucon9-qualify
 make bin/benchmarker
 make bin/benchmark-worker
 make bin/payment
 make bin/shipment
 ./bin/benchmarker
-
-# # 初期画像データダウンロード
-# cd /home/isucon9-qualify/webapp/public
-# curl -LO https://github.com/isucon/isucon9-qualify/releases/download/v2/initial.zip
-#  unzip initial.zip
-#  rm -rf upload
-#  mv v3_initial_data upload
-
-# # ベンチマーク用画像データダウンロード
-# cd /home/isucon9-qualify/initial-data
-# curl -LO https://github.com/isucon/isucon9-qualify/releases/download/v2/bench1.zip
-# unzip bench1.zip
-# rm -rf images
-# mv v3_bench1 images
-
-
-# cd /home/isucon9-qualify
-# make 
-# ./bin/benchmarker
