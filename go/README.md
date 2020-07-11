6784630ef8ec484a902ce9eb5c4ca09d6085d73c
tken2039 <tken2039@gmail.com>
2020-07-11 14:06:39 +0900

    create this branch

--- go/README.md
+++
@@ -1,5 +0,0 @@
-# Ubuntu18.04における導入
-apt install software-properties-common -y
-add-apt-repository ppa:longsleep/golang-backports -y
-apt update
-apt install golang -y
--- vim/setup.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-cp .vimrc ~/.vimrc
-vim +PluginInstall +qall
